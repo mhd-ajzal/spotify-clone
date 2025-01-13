d220f9f6a86620cdedaad41b2563c980d5b583cf
mhd-ajzal <ajzalmuhammed15@gmail.com>
2025-01-13 10:54:35 +0530

    second commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react()],
+	build: {
+	outDir: "public", // Set the output directory to "public"
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
